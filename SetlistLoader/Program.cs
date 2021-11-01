d15d35f5f41bd1b8faf1e20125438153c065f220
gddonoho <92893863+gddonoho@users.noreply.github.com>
2021-10-31 19:29:56 -0700

    initial commit

---
+++ SetlistLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SetlistLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
